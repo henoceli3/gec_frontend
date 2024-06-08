b45808e6b1bf45e65e6a2a39a4fe2ebc6b0f2acc
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-06-08 16:57:47 +0000

    s

--- src/pages/Acceuil.tsx
+++ src/pages/Acceuil.tsx
@@ -6,8 +6,24 @@ const Acceuil = () => {
           height: "50vh",
           width: "100%",
           backgroundImage: "url(/images/11.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.5)",
+          }}
+        >
+          <h1 style={{ color: "white" }}>
+            Une génération de chretiens qui impacte dans le monde
+          </h1>
+        </div>
       </div>
     </>
   );
